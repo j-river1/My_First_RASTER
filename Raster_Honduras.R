12b2814ff84fe164c873b93674175d4afe599e62
Rivera <J.C.RIVERA@cgiar.org>
2018-01-23 08:22:20 -0500

    jcr

--- Raster_Honduras.R
+++ Raster_Honduras.R
@@ -4,7 +4,7 @@ library(raster)
 
 
 
-#Read Raster
+#***Methods Raster Folders***
 #According to final document, there are three types of classification.
 #1. PCA_Kmeans
 #2. PCA_Mclust
@@ -16,4 +16,37 @@ PCA_Mclust ="//dapadfs/workspace_cluster_8/AEPS/HONDURAS/ClasificacionesFinales/
 tnse_GNG ="//dapadfs/workspace_cluster_8/AEPS/HONDURAS/ClasificacionesFinales/Raster_gng_tsne_RedcVariables_Norm_per270_NClu14"
 
 
-raster_kmeans=raster(tnse_GNG)
+#***Weather Raster Folders***
+
+VectorFile="//dapadfs/workspace_cluster_8/AEPS/HONDURAS/SHP_File"
+ElevationFile="//dapadfs/workspace_cluster_8/AEPS/HONDURAS/Elevacion/"
+SoilFiles="//dapadfs/workspace_cluster_8/AEPS/HONDURAS/Suelo/"
+ClimeFiles="//dapadfs/workspace_cluster_8/AEPS/HONDURAS/Clima/WorldClim"
+
+#***Read Raster Weather***
+
+setwd(SoilFiles)
+listFiles=list.files(pattern = "*.tif$")
+soilsInfo=stack(listFiles)
+
+setwd(ClimeFiles)
+listFiles=list.files(pattern = "*.tif$")
+ClimeInfo=stack(listFiles)
+
+setwd(ElevationFile)
+listFiles=list.files(pattern = "*.tif$")
+ElevationInfo=stack(listFiles[1])
+
+raster_kmeans=raster(paste0(tnse_GNG, ".tif"))
+
+#info_raster works for getting information of raster.
+#Arguments.  -name_raster.
+#Return.     -table. With all information 
+
+info_raster <- function (name_raster)
+{
+  raster_info  <- raster(paste0(tnse_GNG, ".tif"))
+  extraction <- data.frame(cbind(extract(ClimeInfo,coordinatesExtract),extract(rasterkmeans,coordinatesExtract)))
+  return (raster_info)
+  
+}
