cb23230bdbc221f97683de4a93123d84c9fdfb9d
Rivera <J.C.RIVERA@cgiar.org>
2018-01-23 10:33:01 -0500

    jcr

--- Raster_Honduras.R
+++ Raster_Honduras.R
@@ -61,19 +61,95 @@ info_raster <- function (name_raster)
   extraction <- data.frame(cbind(extract(ClimeInfo,coordinatesExtract),extract(raster_info,coordinatesExtract)))
   
   #Data Mean Anual 
-  data_Tem_Mean <- data.frame(extraction%>%group_by(V68)%>%summarise(minTemMeanAnual=min(bio_1,na.rm=T),maxTemMeanAnual=max(bio_1,na.rm=T)))
+  data_Tem_Mean_An <- data.frame(extraction%>%group_by(V68)%>%summarise(minTemMeanAnual=min(bio_1,na.rm=T),maxTemMeanAnual=max(bio_1,na.rm=T)))
+  
+  
   
   #Data Temperature Monthly Range
-  data_Tem_Range <- data.frame(extraction%>%group_by(V68)%>%summarise(Mean_Monthly_Range = mean(bio_2)))
+  data_Tem_Range_Mon <- data.frame(extraction%>%group_by(V68)%>%summarise(Mean_Monthly_Range = mean(bio_2)))
+  data_Tem_Range_Mon$V68 <- NULL
+    
+  #Data Range Temperature Anual  Range
+  data_Tem_Range_An_Range <- data.frame(extraction%>%group_by(V68)%>%summarise(Mean_Anual_Range = mean(bio_7)))
+  data_Tem_Range_An_Range$V68 <- NULL
   
-  #Data Temperature Anual  Range
-  data_Tem_Range <- data.frame(extraction%>%group_by(V68)%>%summarise(Mean_Anual_Range = mean(bio_7)))
   
   #Data Precipitaion Anual
   data_Precip <- data.frame(extraction%>%group_by(V68)%>%summarise(Mean_Precipitation_Anual = mean(bio_12)))
+  data_Precip$V68 <- NULL
   
-  data_result <- merge(data_Tem_Range)
   
-  return (data_Precip )
+  #Data Mean January
+  data_tmean_jan <- data.frame(extraction%>%group_by(V68)%>%summarise(Mean_January = mean(tmean_1)))
+  data_tmean_jan$V68 <- NULL
+  
+  
+  
+  #Data Mean February
+  data_tmean_feb <- data.frame(extraction%>%group_by(V68)%>%summarise(Mean_February = mean(tmean_2)))
+  data_tmean_feb$V68 <- NULL
+  
+  
+  #Data Mean March 
+  data_tmean_marc <- data.frame(extraction%>%group_by(V68)%>%summarise(Mean_March = mean(tmean_3)))
+  data_tmean_marc$V68 <- NULL
+  
+  
+  #Data Mean April 
+  data_tmean_apri <- data.frame(extraction%>%group_by(V68)%>%summarise(Mean_April = mean(tmean_4)))
+  data_tmean_apri$V68 <- NULL
+  
+  
+  #Data Mean May 
+  data_tmean_may <- data.frame(extraction%>%group_by(V68)%>%summarise(Mean_May = mean(tmean_5)))
+  data_tmean_may$V68 <- NULL
+  
+  
+  #Data Mean June 
+  data_tmean_jun <- data.frame(extraction%>%group_by(V68)%>%summarise(Mean_June = mean(tmean_6)))
+  data_tmean_jun$V68 <- NULL
+  
+
+  #Data Mean July 
+  data_tmean_jul <- data.frame(extraction%>%group_by(V68)%>%summarise(Mean_July = mean(tmean_7)))
+  data_tmean_jul$V68 <- NULL
+  
+
+  #Data Mean Agust 
+  data_tmean_agus <- data.frame(extraction%>%group_by(V68)%>%summarise(Mean_Agust = mean(tmean_8)))
+  data_tmean_agus$V68 <- NULL
+  
+  
+  #Data Mean Sept 
+  data_tmean_sept <- data.frame(extraction%>%group_by(V68)%>%summarise(Mean_Sept = mean(tmean_9)))
+  data_tmean_sept$V68 <- NULL
+  
+
+  #Data Mean Oct 
+  data_tmean_oct <- data.frame(extraction%>%group_by(V68)%>%summarise(Mean_Oct = mean(tmean_10)))
+  data_tmean_oct$V68 <- NULL
+  
+  
+  #Data Mean Nov 
+  data_tmean_Nov <- data.frame(extraction%>%group_by(V68)%>%summarise(Mean_Nov = mean(tmean_11)))
+  data_tmean_Nov$V68 <- NULL
+  
+  
+  #Data Mean Dic 
+  data_tmean_Dic <- data.frame(extraction%>%group_by(V68)%>%summarise(Mean_Dic = mean(tmean_12)))
+  data_tmean_Dic$V68 <- NULL
+  
+  
+  
+  
+  #list 
+  
+  resulta <- list (data_Tem_Mean_An , data_Tem_Range_Mon, data_Tem_Range_An_Range,data_Precip, data_tmean_jan, data_tmean_feb, data_tmean_marc,  data_tmean_apri, data_tmean_may , data_tmean_jun, 
+                   data_tmean_jul, data_tmean_agus, data_tmean_sept, data_tmean_oct, data_tmean_Nov, data_tmean_Dic)
+  
+  prueba <- do.call("cbind", resulta)
+  
+  
+  return (prueba )
   
 }
