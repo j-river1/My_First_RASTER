5c8db602504576f19b4abc6ba05347cc51144c01
Juan Camilo Rivera P <lacicladelosdos@gmail.com>
2018-02-06 07:59:45 -0500

    jcr

--- join_files.R
+++ join_files.R
@@ -5,6 +5,8 @@
 #-Return: split of name file 
 
 
+
+
 split_name <- function(filename)
 {
   split_name <- unlist(strsplit(filename, split='_', fixed=TRUE))
@@ -23,8 +25,40 @@ split_name <- function(filename)
 join_files <- function(name_method, num_cluter)
 {
   
+  #Read files Curve Tx and TM
+  graph_all_station_Curve_TX_TM(name_method)
+
+  
+  #Read files Orbothermic
+  graph_all_station (name_method)
+  
+  #Read Soil texture  
+  graph_all_texture_clus(name_method)
+  
+  #Elevation
+  graph_all_elevation_complete(name_method)
+  
+  #Excel texture 
+  soil_texture(name_method) 
+  
+  #Create folder fo each folder     
+  for (i in 1:num_cluter)
+  {
+    mainDir <- paste0(getwd(), "/Data_Final")
+    dir.create(file.path(mainDir, paste0("Cluster_", i)), showWarnings = FALSE)
+    
+  }
+  
+
+  for (i in 1:num_cluter)
+  {
+    list_files_Curve <- list.files("./Temperature_curve", pattern =paste0(i, ))
+    
+  }
+  
   
   
   
   
 }
+
